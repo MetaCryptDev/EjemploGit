9bea421708032431ed7aedf3ed9f67c0eb37cc4a
MetaCryptDev <alberpinigon@gmail.com>
2023-04-11 10:39:30 +0200

    Modifico la clase una segunda vez

--- HolaGit.java
+++ HolaGit.java
@@ -4,7 +4,8 @@ public class HolaGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Git");
-		System.out.println("Cambiamos la clase");
+		System.out.println("Primer Cambio");
+		System.out.println("Segundo Cambio");
 	}
 
 }
